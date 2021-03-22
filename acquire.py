341edd92a894c302d9cb97def0cc366855710766
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2021-03-21 21:02:19 -0500

    Updated query. Finished prepare.py and began final notebook.

--- acquire.py
+++ acquire.py
@@ -3,16 +3,15 @@ import env
 from utilities import generate_db_url, generate_df
 
 _zillow_query = """
-SELECT bathroomcnt, bedroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, taxamount, regionidcounty
+SELECT bathroomcnt, bedroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, taxamount, fips
 	FROM properties_2017
 		JOIN predictions_2017 USING(parcelid)
-	WHERE (transactiondate LIKE \'2017-05-%%\' 
-		OR transactiondate LIKE \'2017-08-%%\')
-		AND propertylandusetypeid = 261
+	WHERE (transactiondate BETWEEN '2017-05-01' AND '2017-08-31')
+		AND (unitcnt = 1 OR propertylandusetypeid IN (261, 279, 262, 263, 264, 266, 275))
 		AND calculatedfinishedsquarefeet IS NOT NULL
 		AND bathroomcnt IS NOT NULL
 		AND bedroomcnt IS NOT NULL
-		AND regionidcounty IS NOT NULL
+		AND fips IS NOT NULL
 		AND taxvaluedollarcnt IS NOT NULL
 		AND taxamount IS NOT NULL;
 """
---
+++ prepare.py
@@ -0,0 +1,59 @@
+import env
+
+from utilities import split_dataframe_continuous_target, generate_xy_splits
+
+def _clean_zillow(df):
+    zillow_df = df.copy()
+    
+    zillow_df = zillow_df.rename(columns={'bathroomcnt' : 'bathrooms', 'bedroomcnt' : 'bedrooms', 'calculatedfinishedsquarefeet' : 'total_sqft', 'taxvaluedollarcnt' : 'tax_value', 'taxamount' : 'tax_amount'})
+    
+    return _remove_outliers(zillow_df)
+    
+def _remove_outliers(zillow_df):
+    
+    # Bathroom outliers
+    q1_bath = zillow_df.bathrooms.quantile(.25)
+    q3_bath = zillow_df.bathrooms.quantile(.75)
+    iqr_bath = q3_bath - q1_bath
+
+    upper_bath = q3_bath + (1.5 * iqr_bath)
+    lower_bath = q1_bath - (1.5 * iqr_bath)
+
+    # Bedroom outliers
+    q1_beds = zillow_df.bedrooms.quantile(.25)
+    q3_beds = zillow_df.bedrooms.quantile(.75)
+    iqr_beds = q3_beds - q1_beds
+
+    upper_beds = q3_beds + (1.5 * iqr_beds)
+    lower_beds = q1_beds - (1.5 * iqr_beds)
+    
+    # Sqft outliers
+    q1_sqft = zillow_df.total_sqft.quantile(.25)
+    q3_sqft = zillow_df.total_sqft.quantile(.75)
+    iqr_sqft = q3_sqft - q1_sqft
+
+    upper_sqft = q3_sqft + (1.5 * iqr_sqft)
+    lower_sqft = q1_sqft - (1.5 * iqr_sqft)
+    
+    # Tax value outliers
+    q1_value = zillow_df.tax_value.quantile(.25)
+    q3_value = zillow_df.tax_value.quantile(.75)
+    iqr_value = q3_value - q1_value
+
+    upper_value = q3_value + (1.5 * iqr_value)
+    lower_value = q1_value - (1.5 * iqr_value)
+    
+    non_outliers = (zillow_df.bathrooms > lower_bath) & (zillow_df.bathrooms < upper_bath) & (zillow_df.bedrooms > lower_beds) & (zillow_df.bedrooms < upper_beds) & (zillow_df.total_sqft > lower_sqft) & (zillow_df.total_sqft < upper_sqft) & (zillow_df.tax_value > lower_value) & (zillow_df.tax_value < upper_value)
+    
+    return zillow_df[non_outliers]
+
+def prepare_zillow(unprepared_zillow):
+    prepped_data = {}
+    
+    clean_zillow_df = _clean_zillow(unprepared_zillow)
+    prepped_data['population'] = clean_zillow_df
+    
+    train, validate, test = split_dataframe_continuous_target(clean_zillow_df, 'tax_value')
+    prepped_data['samples'] = generate_xy_splits(train, validate, test, 'tax_value', drop_columns=['fips', 'tax_amount'])
+    
+    return prepped_data 
